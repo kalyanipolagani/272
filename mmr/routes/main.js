850b3e1089d80907f75016c5f6819f06e1948c54
kalyanipolagani <kalyani.polagani@sjsu.edu>
2016-06-17 09:41:28 -0700

    Abhi Project

---
+++ mmr/routes/main.js
@@ -0,0 +1,12 @@
+
+/*
+ * GET home page.
+ */
+
+exports.main = function(req, res){
+  res.render('main', { title: 'Express' });
+};
+
+exports.main1 = function(req, res){
+  res.render('main1', { title: 'Express' });
+};
